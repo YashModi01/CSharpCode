20dfae59fd82955e01b2d8e80541472a778370f1
YashModi01 <modiyash996@gmail.com>
2021-01-21 03:29:54 -0600

    created a mad lib

--- Newproject/Program.cs
+++ Newproject/Program.cs
@@ -62,6 +62,25 @@ namespace Newproject
 
             Console.WriteLine(num1 + num2);*/
 
+            // Mad lib
+
+            string color, noun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a noun: ");
+            noun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(noun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+
+
 
 
 
